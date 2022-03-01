30521c4da437b662a9bd8f7d5abdecab99d8fba7
Dmitry_Kulik <dima27121985@gmail.com>
2022-03-01 17:02:52 +0300

    Добавьте файлы проекта.

---
+++ module4/Program.cs
@@ -0,0 +1,20 @@
+﻿
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 0 && number < 31)
+{
+    Console.WriteLine($"Число в промежутке от 0 до 30.");
+}
+else if (number > 30 && number < 61)
+{
+    Console.WriteLine($"Число в промежутке от 31 до 60.");
+}
+else if (number > 60 && number < 91)
+{
+    Console.WriteLine($"Число в промежутке от 61 до 90."); ; ;
+}
+else if (number > 90 || number < 0)
+{
+    Console.WriteLine($"Я такого числа не знаю."); ; ;
+}
---
+++ task2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+public class Exercise39
+{
+    public static void Main()
+    {
+        int i = 200;
+        Console.Write("Числа между 200 и 500, которые делятся на 17 : \n");
+        while (i < 500) 
+
+        {
+            i++;
+            if (i % 17 == 0)
+            {
+                Console.Write("{0} , ", i);
+            }
+        }
+    }
+}
